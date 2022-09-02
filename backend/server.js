fe575b3b59cc810792febbd1447da1573da271e2
rk_man <naveenbadi2003@gmail.com>
2022-09-02 12:01:33 +0530

    setting up heroku in the app

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,8 @@
 const mongoose = require("mongoose");
 const dotenv = require("dotenv");
+const express = require("express");
 dotenv.config({ path: "./config.env" });
+const path = require("path");
 const PORT = process.env.PORT || 5000;
 
 const DB_CON_STRING = process.env.DB_CON_STRING.replace(
@@ -18,6 +20,18 @@ mongoose
     });
 
 const app = require("./app");
+
+if (process.env.NODE_ENV === "production") {
+    //set static files folder
+    app.use(express.static(path.join(__dirname, "../frontend/build")));
+
+    //display the static file
+    app.get("*", (req, res) => {
+        res.sendFile(
+            path.resolve(__dirname, "../", "frontend", "build", "index.html")
+        );
+    });
+}
 
 app.listen(PORT, () => {
     console.log(`server is listening at port ${PORT}`);
